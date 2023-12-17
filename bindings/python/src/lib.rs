fb61f2ecb3b82d820749ab84851bbb209e5cf74c
Brian Li <me@brian14708.dev>
2023-12-18 01:11:51 +0800

    Initial python wrapper

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -1,14 +1,24 @@
-use pyo3::prelude::*;
+use ::promptkit::script::{InputValue, Script};
+use pyo3::{
+    exceptions::{PyRuntimeError, PyValueError},
+    prelude::*,
+};
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
 fn run_script(script: &str, json: &str) -> PyResult<String> {
-    Ok(json.to_string())
+    // Ok(json.to_string())
+    let v = serde_json::from_str(json).unwrap();
+
+    let ret = Script::new(script)
+        .map_err(|e| PyValueError::new_err(e.to_string()))?
+        .run("handle", [InputValue::Json(v)], [])
+        .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
+
+    serde_json::to_string(&ret).map_err(|e| PyRuntimeError::new_err(e.to_string()))
 }
 
-/// A Python module implemented in Rust.
 #[pymodule]
-fn python(_py: Python, m: &PyModule) -> PyResult<()> {
+fn promptkit(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(run_script, m)?)?;
     Ok(())
 }
