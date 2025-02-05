7193e8dc491636055895e47ea098b2030f365488
Brian Li <me@brian14708.dev>
2025-02-05 15:22:24 +0800

    refactor: update prometheus handler

--- crates/server/src/routes/mod.rs
+++ crates/server/src/routes/mod.rs
@@ -3,36 +3,33 @@ mod state;
 
 use std::{future::ready, time::Duration};
 
-use axum::{http::StatusCode, response::Response, routing::get};
+use axum::{http::StatusCode, routing::get};
 pub use env::VmEnv;
-use http::header::CONTENT_TYPE;
-use metrics_exporter_prometheus::PrometheusBuilder;
+use metrics_exporter_prometheus::{PrometheusBuilder, PrometheusHandle};
 pub use state::AppState;
 use tower_http::services::{ServeDir, ServeFile};
 
 pub fn router(state: &AppState) -> axum::Router {
     let prometheus = PrometheusBuilder::new().install_recorder().unwrap();
-    let m = prometheus.clone();
-    tokio::spawn(async move {
-        loop {
-            tokio::time::sleep(Duration::from_secs(5)).await;
-            m.run_upkeep();
-        }
-    });
+    tokio::spawn(prometheus_upkeep(
+        prometheus.clone(),
+        Duration::from_secs(5),
+    ));
 
     axum::Router::new()
         .route("/debug/healthz", get(|| ready(StatusCode::NO_CONTENT)))
-        .route(
-            "/debug/metrics",
-            get(move || {
-                let mut resp = Response::new(prometheus.render());
-                resp.headers_mut().insert(CONTENT_TYPE, "".parse().unwrap());
-                ready(resp)
-            }),
-        )
+        .route("/debug/metrics", get(move || ready(prometheus.render())))
         .with_state(state.clone())
         .nest_service(
             "/ui",
             ServeDir::new("ui/build").fallback(ServeFile::new("ui/build/404.html")),
         )
+}
+
+async fn prometheus_upkeep(handle: PrometheusHandle, duration: Duration) {
+    let mut interval = tokio::time::interval(duration);
+    loop {
+        interval.tick().await;
+        handle.run_upkeep();
+    }
 }
