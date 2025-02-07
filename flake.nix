c1e8ed537ab5f38c759842892f7b9874d31d3e55
Brian Li <me@brian14708.dev>
2025-02-08 00:04:16 +0800

    feat: avoid fhs in devshell

--- flake.nix
+++ flake.nix
@@ -1,76 +1,88 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    crane.url = "github:ipetkov/crane";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.rust-analyzer-src.follows = "";
     };
   };
-
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      fenix,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        rust-toolchain = (
-          with fenix.packages.${system};
-          combine [
-            (stable.withComponents [
-              "cargo"
-              "clippy"
-              "rustc"
-              "rustfmt"
-            ])
-            targets.wasm32-wasip1.stable.rust-std
-          ]
-        );
-      in
-      {
-        formatter = pkgs.nixfmt-rfc-style;
-        devShells.default =
-          (pkgs.buildFHSEnv {
-            name = "devshell";
-            targetPkgs =
-              pkgs:
-              with pkgs;
-              [
-                nodejs
-                pnpm
-                svelte-language-server
+    inputs@{ flake-parts, crane, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          rust-toolchain = (
+            with inputs'.fenix.packages;
+            combine [
+              (stable.withComponents [
+                "cargo"
+                "clippy"
+                "rustc"
+                "rustfmt"
+              ])
+              targets.wasm32-wasip1.stable.rust-std
+            ]
+          );
+          craneLib = (crane.mkLib pkgs).overrideToolchain rust-toolchain;
+        in
+        {
 
-                (python313.withPackages (
-                  p: with p; [
-                    cython
-                    setuptools
-                    uv
+          devShells.default =
+            (craneLib.devShell.override {
+              mkShell = pkgs.mkShell.override {
+                stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+              };
+            })
+              {
+                buildInputs =
+                  with pkgs;
+                  [
+                    nodejs
+                    pnpm
+                    svelte-language-server
+
+                    (python313.withPackages (
+                      p: with p; [
+                        cython
+                        setuptools
+                        uv
+                      ]
+                    ))
+
+                    cmake
+                    ninja
+                    buf
+                    protobuf_28
+                    pkg-config
+                    gcc
+                    rust-analyzer
+                    rust-toolchain
                   ]
-                ))
-
-                cmake
-                ninja
-                buf
-                protobuf_28
-                pkg-config
-                gcc
-                rust-analyzer
-                rust-toolchain
-              ]
-              ++ lib.optional stdenv.isDarwin [
-                darwin.apple_sdk.frameworks.Security
-                darwin.apple_sdk.frameworks.CoreFoundation
-                darwin.apple_sdk.frameworks.SystemConfiguration
-                libiconv
-              ];
-          }).env;
-      }
-    );
+                  ++ lib.optional stdenv.isDarwin [
+                    darwin.apple_sdk.frameworks.Security
+                    darwin.apple_sdk.frameworks.CoreFoundation
+                    darwin.apple_sdk.frameworks.SystemConfiguration
+                    libiconv
+                  ];
+              };
+        };
+    };
 
 }
