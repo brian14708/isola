59b2ae439a5033c4e475a78e3943a162050b4aa8
Brian Li <me@brian14708.dev>
2025-02-21 01:25:44 +0800

    chore: remove unused deps

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,16 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		tailwindcss(),
-		Icons({
-			compiler: "svelte",
-		}),
-	],
+	plugins: [sveltekit(), tailwindcss()],
 	server: {
 		proxy: {
 			"/v1/": {
