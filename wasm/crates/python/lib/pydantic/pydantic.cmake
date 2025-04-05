c6cdd669838f217a008172748e10834f187226c5
Brian Li <me@brian14708.dev>
2025-04-05 16:24:06 +0800

    fix: update pydantic pyo3

--- wasm/crates/python/lib/pydantic/pydantic.cmake
+++ wasm/crates/python/lib/pydantic/pydantic.cmake
@@ -5,9 +5,9 @@ find_package(Python3 ${PYTHON_VERSION})
 
 FetchContent_Declare(
   pydantic-src
-  URL "https://github.com/pydantic/pydantic-core/archive/refs/tags/v2.33.0.tar.gz"
+  URL "https://github.com/pydantic/pydantic-core/archive/0a5bbfc116c1659ce476fac56a44cda96673dfa8.tar.gz"
   URL_HASH
-    SHA256=8ddef852cf968f3b5a857db1749f898ac5209deea78681dc1b8ae018341dbf27
+    SHA256=0937d756b26638acbe995c182765f28b6efea9481850b0bfd2e131e55763f8ea
   DOWNLOAD_DIR ${WASMLIB_DOWNLOAD_DIR}
   PATCH_COMMAND
     patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/pydantic.patch
@@ -32,5 +32,5 @@ ExternalProject_Add(
       "RUSTFLAGS=-Clink-args=-L${CMAKE_BINARY_DIR} -Clink-args=-L${WASMLIB_SYSROOT}/lib/wasm32-wasip1 -Clink-self-contained=no -Crelocation-model=pic" --
     maturin build --release --target wasm32-wasip1 -i python3.13 --out dist
   INSTALL_COMMAND
-    cd ${CMAKE_BINARY_DIR}/pythonpkgs/lib/python3.13/site-packages && cmake -E tar xvf <SOURCE_DIR>/dist/pydantic_core-2.33.0-cp313-cp313-any.whl
+    cd ${CMAKE_BINARY_DIR}/pythonpkgs/lib/python3.13/site-packages && cmake -E tar xvf <SOURCE_DIR>/dist/pydantic_core-2.33.1-cp313-cp313-any.whl
   DEPENDS python)
