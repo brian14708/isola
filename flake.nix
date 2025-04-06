355c8b5bbc85ed6b9af9ea6532b2a418a04b7e22
Brian Li <me@brian14708.dev>
2025-04-06 14:13:08 +0800

    chore: upgrade to rust 1.86

--- flake.nix
+++ flake.nix
@@ -1,100 +1,85 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-utils.url = "github:numtide/flake-utils";
     crane.url = "github:ipetkov/crane";
-    fenix = {
-      url = "github:nix-community/fenix";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.rust-analyzer-src.follows = "";
     };
   };
   outputs =
-    inputs@{ flake-parts, crane, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
-      perSystem =
-        {
-          config,
-          self',
-          inputs',
-          pkgs,
-          system,
-          ...
-        }:
-        let
-          rust-toolchain = (
-            with inputs'.fenix.packages;
-            combine [
-              (stable.withComponents [
-                "cargo"
-                "clippy"
-                "rustc"
-                "rustfmt"
-                "rust-src"
-              ])
-              targets.wasm32-wasip1.stable.rust-std
-            ]
-          );
-          craneLib = (crane.mkLib pkgs).overrideToolchain rust-toolchain;
-        in
-        {
-
-          devShells.default =
-            (craneLib.devShell.override {
-              mkShell = pkgs.mkShell.override {
-                stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.clangStdenv;
-              };
-            })
-              {
-                LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath ([ ]);
-                buildInputs =
-                  with pkgs;
-                  [
-                    just
-
-                    # js
-                    nodejs
-                    pnpm
-                    svelte-language-server
-
-                    # python
-                    (python313.withPackages (
-                      p: with p; [
-                        cython
-                        setuptools
-                        uv
-                        typing-extensions
-                        pip
-                        wheel
-                      ]
-                    ))
-                    pyright
-                    maturin
-
-                    # rust / c++
-                    binaryen
-                    cmake
-                    ninja
-                    buf
-                    protobuf_28
-                    pkg-config
-                    rust-analyzer
-                    rust-toolchain
-                  ]
-                  ++ lib.optional stdenv.isDarwin [
-                    darwin.apple_sdk.frameworks.Security
-                    darwin.apple_sdk.frameworks.CoreFoundation
-                    darwin.apple_sdk.frameworks.SystemConfiguration
-                    libiconv
-                  ];
-              };
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      crane,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
         };
-    };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain (
+          p:
+          p.rust-bin.stable.latest.default.override {
+            targets = [ "wasm32-wasip1" ];
+          }
+        );
+      in
+      {
+        devShells.default =
+          (craneLib.devShell.override {
+            mkShell = pkgs.mkShell.override {
+              stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.clangStdenv;
+            };
+          })
+            {
+              buildInputs =
+                with pkgs;
+                [
+                  just
+
+                  # js
+                  nodejs
+                  pnpm
+                  svelte-language-server
+
+                  # python
+                  (python313.withPackages (
+                    p: with p; [
+                      cython
+                      setuptools
+                      uv
+                      typing-extensions
+                      pip
+                      wheel
+                    ]
+                  ))
+                  pyright
+                  maturin
+
+                  # rust / c++
+                  binaryen
+                  cmake
+                  ninja
+                  buf
+                  protobuf_28
+                  pkg-config
+                  rust-analyzer
+                ]
+                ++ lib.optional stdenv.isDarwin [
+                  darwin.apple_sdk.frameworks.Security
+                  darwin.apple_sdk.frameworks.CoreFoundation
+                  darwin.apple_sdk.frameworks.SystemConfiguration
+                  libiconv
+                ];
+            };
+      }
+    );
 
 }
