c8c283702949ef53d4966c0633de9c8607f78229
Brian Li <me@brian14708.dev>
2025-12-05 20:44:07 +0800

    chore(deps): update dependencies

--- nix/pkgs/ui.nix
+++ nix/pkgs/ui.nix
@@ -16,7 +16,7 @@ stdenv.mkDerivation (finalAttrs: {
   pnpmDeps = pnpm.fetchDeps {
     inherit (finalAttrs) pname version src;
     fetcherVersion = 2;
-    hash = "sha256-FECxZrKcrpjM0wZDqChgdgDBpF4F8f1A93elXhkvmMw=";
+    hash = "sha256-xttsBSUoYdHSqw2jnK3CdUcxK0g4vMhZEzF4pC9331w=";
   };
 
   nativeBuildInputs = [
